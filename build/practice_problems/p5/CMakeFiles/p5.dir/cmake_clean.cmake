c5839efbfef48bba3a9ddb601818952ae918fbcf
Karthikkumarvasudeva <karthik.vasueva@ttu.edu>
2024-12-09 21:39:26 -0600

    p5 file

---
+++ build/practice_problems/p5/CMakeFiles/p5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p5.dir/p5.cc.o"
+  "CMakeFiles/p5.dir/p5.cc.o.d"
+  "p5"
+  "p5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
